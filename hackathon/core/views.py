36594dcf2472ee3383d67e73d5feb256e94ab491
aposgial <apostolosgls@mail.com>
2023-05-06 15:06:18 +0300

    shape generator firt trial

--- hackathon/core/views.py
+++ hackathon/core/views.py
@@ -22,3 +22,49 @@ def view(reqest):
         
     return render(reqest,'grid.html', context=context)
 
+def shape_generator(shape_form:str):
+    if not shape_form:
+        return None
+    
+    rows, cols = (5, 5)
+    arr = [[0]*cols]*rows
+
+    if shape_form == 'F':
+        arr[2][2] = 1
+        arr[2][3] = 1
+        arr[3][1] = 1
+        arr[3][2] = 1
+        arr[4][2] = 1
+        return arr
+
+    if shape_form == 'I':
+        for index in range(5):
+            arr[2][index] = 1
+        return arr
+    
+    if shape_form == 'L':
+        for index in range(4):
+            arr[2][1 + index] = 1
+        arr[3][4] = 1
+        return arr
+    
+
+def shape(reqest):
+    if reqest.method == 'GET':
+        try:
+            arr = shape_generator(str(reqest.GET.get('option')))
+            axis_flag = True
+        except (TypeError, ArithmeticError):
+            X_axis = 0
+            Y_axis = 0
+            axis_flag = False
+            message = 'input error'
+        except Exception:
+            message = 'wasted'
+
+        context:dict = {'arr': arr,
+                        'axis_flag': axis_flag,
+                        'message': message
+                        }
+        
+    return render(reqest,'grid.html', context=context)

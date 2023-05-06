0efc43247d041ee9024b026ce142cae1317c5100
aposgial <apostolosgls@mail.com>
2023-05-06 13:00:42 +0300

    method view render content

--- hackathon/core/views.py
+++ hackathon/core/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def view(reqest):
-    return render(reqest,'grid.html')
+    if reqest.method == 'GET':
+        X_axis = int(reqest.GET.get('X'))
+        Y_axis = int(reqest.GET.get('Y'))
+
+        context:dict = {'X_axis':X_axis,
+                        'Y_axis':Y_axis
+                        }
+        
+    return render(reqest,'grid.html', context=context)
 

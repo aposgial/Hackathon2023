fa0e44575c026747a48a4d877bc22818e92e84ae
MichalisPapa19 <michpapaioannou93@gmail.com>
2023-05-06 13:25:27 +0300

    com

--- hackathon/core/views.py
+++ hackathon/core/views.py
@@ -3,22 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def view(reqest):
-    if reqest.method == 'GET':
-        try:
-            X_axis = int(reqest.GET.get('X'))
-            Y_axis = int(reqest.GET.get('Y'))
-            axis_flag = True
-        except (TypeError, ArithmeticError):
-            X_axis = 0
-            Y_axis = 0
-            axis_flag = False
-            message = 'input error'
-
-        context:dict = {'X_axis': X_axis,
-                        'Y_axis': Y_axis,
-                        'axis_flag': axis_flag,
-                        'message': message
-                        }
-        
-    return render(reqest,'grid.html', context=context)
+    return render(reqest,'grid.html')
 

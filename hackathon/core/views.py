56cc97d52a5231df667864504fe8d515335f6709
fragos5 <mitsosfragos@gmail.com>
2023-05-07 10:30:30 +0300

    comm

---
+++ hackathon/core/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+from .controller import Controller
+from .exceptions import *
+from hackathon.mixins import ReversableList
+from .forms import CHOICES
+from django.utils import datastructures as ds
+
+# Create your views here.
+def view(request):
+    if request.method == 'GET':
+        try:
+            X_axis = [None] * int(request.GET.get('X'))
+            Y_axis = [None] * int(request.GET.get('Y'))
+            axis_flag = True
+            message = 'okk'
+        except (TypeError, ArithmeticError, Exception):
+            X_axis = 0
+            Y_axis = 0
+            axis_flag = False
+            message = 'input error'
+
+        context:dict = {'X_axis': X_axis,
+                        'Y_axis': Y_axis,
+                        'axis_flag': axis_flag,
+                        'message': message
+                        }
+        
+    return render(request,'grid.html', context=context)
+
+
+def rotate(request):
+    controller = Controller()
+    form = CHOICES(request.POST)
+    data = request.POST
+
+    try:
+        original = controller.shape_generator(str(data['NUMS']))
+        if data['array'] != controller.shape_rotatior(original['arr']):
+            rotated = controller.shape_rotatior(original['arr'])
+        else:
+            print('problem')
+            rotated = original['arr']
+
+        print(data['array'])
+        print(original['arr'])
+        print(rotated)
+    except ds.MultiValueDictKeyError:
+        return render(request,'rotates.html', {'form':form})
+
+    context:dict ={
+                    'arr_info':{'arr':rotated,
+                                'photo':original['photo']},
+                    'axis_flag': True,
+                    'message': "Hi",
+                    'form':form
+    }
+    return render(request,'rotates.html', context=context)
+
+def shape(reqest):
+    controller = Controller()
+    if reqest.method == 'GET':
+        try:
+            arr_info = controller.shape_generator(str(reqest.GET.get('option')))
+            axis_flag = True
+            message = 'ok'
+        except (TypeError, ArithmeticError):
+            axis_flag = False
+            message = 'input error, give only latters'
+        except EmplyParameterError:
+            axis_flag = False
+            message = 'give a later'
+        
+        context:dict = {'arr_info': arr_info,
+                        'axis_flag': axis_flag,
+                        'message': message
+                        }
+
+        
+    return render(reqest,'shapes.html', context=context)
